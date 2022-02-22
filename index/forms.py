63fe04a66f8bae658cea56e31873966a38b772f2
Simarjeet <sjthecoder@gmail.com>
2022-02-22 19:59:01 +0530

    send mail backend complete

--- index/forms.py
+++ index/forms.py
@@ -1,6 +1,5 @@
-from pyexpat.errors import messages
 from django import forms
-
+import smtplib
 
 class Contact(forms.Form):
     email = forms.EmailField(widget=forms.TextInput(
@@ -14,3 +13,21 @@ class Contact(forms.Form):
         attrs={'placeholder': 'Subject', 'autocomplete': 'off'}))
     message = forms.CharField(max_length=300, widget=forms.Textarea(
         attrs={'placeholder': 'Message', 'autocomplete': 'off'}))
+
+    def send_email(self, user, pwd, recipient, subject, body):
+        FROM = user
+        TO = recipient if isinstance(recipient, list) else [recipient]
+        SUBJECT = subject
+        TEXT = body
+
+        # Prepare actual message
+        message = f"From: {FROM}\nTo: {TO}\nSubject: {SUBJECT}\n\n{TEXT}"
+        try:
+            server = smtplib.SMTP("smtp.gmail.com", 587)
+            server.starttls()
+            server.login(user, pwd)
+            server.sendmail(FROM, TO, message)
+            server.close()
+            print('successfully sent the mail')
+        except:
+            print("failed to send mail")
--- index/views.py
+++ index/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from . forms import Contact
 from django.contrib import messages
+import os
 
 
 # Create your views here.
@@ -23,8 +23,22 @@ def contact(requests):
     if requests.method == 'POST':
         form = Contact(requests.POST)
         if form.is_valid():
-            messages.success(requests, 'Thank you for contacting me')
-
+            person_email = requests.POST.get('email')
+            person_name = requests.POST.get('name')
+            person_subject = requests.POST.get('subject')
+            person_message = requests.POST.get('message')
+            email_to = os.environ.get('EMAIL_TO')
+            user = os.environ.get('EMAIL')
+            password = os.environ.get('EMAIL_PASSWORD')
+            try:
+                form.send_email(
+                    user=user, pwd=password, recipient=email_to,
+                    subject=person_subject,
+                    body=f'Mail from\nName:{person_name}\nEmail:{person_email}\nMessage:{person_message}')
+                messages.success(requests, 'Thank you for contacting me')
+            except:
+                messages.ERROR(
+                    requests, 'There was some error sending message')
     else:
         form = Contact()
     return render(requests, 'index/contact.html', {
