4ab159493b8d1f63164e0aff358ea794bb517983
Simarjeet <sjthecoder@gmail.com>
2022-02-23 04:58:28 +0530

    remove popup from blog page

--- static-production/js/components/bundle.js
+++ static-production/js/components/bundle.js
@@ -145,9 +145,6 @@ function contactJS() {
 function blogJS() {
     "use strict";
 
-    const popBlog = document.getElementById("pop_blog");
-    const closeBnt = document.getElementById("close_btn");
-    const overlay = document.querySelector(".overlay");
     const fetchBtn = document.querySelector(".fetch-btn");
     const clearFilter = document.querySelector(".filter_clear");
     const filterUnhideBtn = document.querySelector(".filter_hide");
@@ -170,7 +167,6 @@ function blogJS() {
     function updateBlogs(data) {
         const blogs = document.querySelector(".blogs");
         blogs.innerHTML = data;
-        addPopup();
         fetchBtn.innerHTML = "Search";
     }
 
@@ -183,7 +179,6 @@ function blogJS() {
                 blogs.insertAdjacentHTML("beforeend", data);
             }
         }
-        addPopup();
     }
 
     function getCookie(name) {
@@ -202,65 +197,6 @@ function blogJS() {
             }
         }
         return cookieValue;
-    }
-
-    function unhide() {
-        popBlog.classList.remove("pop_blog_hidden");
-
-        let image = document.createElement("img");
-        image.classList.add("popup_img");
-        image.src = this.children[0].src;
-
-        let title = createElementWithClassInnerHTML(
-            "h1",
-            "popup_title",
-            this.children[1].innerHTML
-        );
-        let tags = createElementWithClassInnerHTML(
-            "ul",
-            "popup_tags",
-            this.children[2].innerHTML
-        );
-        let content = createElementWithClassInnerHTML(
-            "div",
-            "popup_content",
-            this.children[3].innerHTML
-        );
-        let publishDate = createElementWithClassInnerHTML(
-            "div",
-            "popup_date",
-            this.children[4].innerHTML
-        );
-
-        let href = document.createElement("a");
-        href.classList.add("popup_page_link");
-        href.innerHTML = "Page";
-        href.target = "_blank";
-        href.href = this.children[5].href;
-
-        // console.log(image, title, tags, content, publishDate, href.href);
-
-        popBlog.appendChild(title);
-        popBlog.appendChild(image);
-        popBlog.appendChild(tags);
-        popBlog.appendChild(content);
-        popBlog.appendChild(publishDate);
-        popBlog.appendChild(href);
-        overlay.classList.add("overlay_on");
-    }
-
-    function hide() {
-        popBlog.classList.add("pop_blog_hidden");
-        popBlog.innerHTML = '<div id="close_btn" >&#10005;</div>';
-        document.getElementById("close_btn").onclick = hide;
-        overlay.classList.remove("overlay_on");
-    }
-
-    function addPopup() {
-        const blogs = document.querySelectorAll(".blog");
-        blogs.forEach(function (blog) {
-            blog.addEventListener("click", unhide);
-        });
     }
 
     function clearFilters() {
@@ -327,22 +263,15 @@ function blogJS() {
         }
     }
 
-    closeBnt.onclick = hide;
-
-    overlay.onclick = hide;
-
-    addPopup();
+    filterUnhideBtn.addEventListener("click", function () {
+        document.getElementById("filter").classList.toggle("hide_filter");
+    });
 
     fetchBtn.addEventListener("click", getPost);
 
     clearFilter.addEventListener("click", clearFilters);
 
-    filterUnhideBtn.addEventListener("click", function () {
-        document.getElementById("filter").classList.toggle("hide_filter");
-    });
-
     const blogScroll = document.querySelector(".blogs");
-    // console.log(blogScroll);
 
     let loadingBLogs = false;
 
