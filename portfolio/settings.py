3ef5d97693266b187fad72f25da34c9d9f474d48
Simarjeet <sjthecoder@gmail.com>
2022-03-09 18:53:59 +0530

    settings.py TinyMCE fix#5

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -135,7 +135,7 @@ if not(DEBUG):
     GS_BUCKET_NAME = os.environ.get('BUCKET_NAME')
     STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
     CLOUDRUN_SERVICE_URL = os.environ.get("CLOUDRUN_SERVICE_URL", False)
-    TINYMCE_JS_URL = os.path.join(STATIC_URL, "tiny_mce/tinymce.min.js")
+    TINYMCE_JS_URL = os.path.join(STATIC_URL, "tinymce/tinymce.min.js")
     TINYMCE_JS_ROOT = os.path.join(STATIC_URL, "tinymce/")
     ALLOWED_HOSTS = ['*']
     # CSRF_TRUSTED_ORIGINS = ['https://simarjeet.in/']
